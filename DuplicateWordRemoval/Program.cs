aa0bbfba0f8e174d37ff1bbf9c9acce4a1b005f9
smith <a@b.c>
2018-04-18 19:30:16 -0400

    uploading folder

---
+++ DuplicateWordRemoval/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateWordRemoval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Sentence but without punctuation marks!");
+            string line =Console.ReadLine();
+            string[] words = line.Split(' ');
+            
+            var filtered =
+                from element in words
+                let lowercaseString =element.ToLower()
+                orderby lowercaseString
+                select lowercaseString;
+
+            Console.WriteLine("Your Sentence without duplicate words and ordered alphabetically");
+            foreach (var element in filtered.Distinct())
+            {
+                Console.Write(" " + element);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
